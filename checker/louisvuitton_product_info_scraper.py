7c49e34681b2775e0340cdd77bacabc86e4680d9
djshin443 <132671614+djshin443@users.noreply.github.com>
2024-04-17 23:35:14 +0900

    Update louisvuitton_product_info_scraper_v0.4.py
    콘솔에서 깔끔하게 출력

--- checker/louisvuitton_product_info_scraper.py
+++ checker/louisvuitton_product_info_scraper.py
@@ -1,4 +1,4 @@
-import os
+import os  # 필요한 경우 이미 코드에 포함되어 있어요.
 import sys
 import json
 import re
@@ -12,28 +12,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from colorama import Fore, Style
+import textwrap
+
 
 # 사용자 정의 상태 변수
-ON_WEBSITE_SOLD = "O"
-NOT_ON_WEBSITE_SOLD = "X"
-NOT_AVAILABLE = "N/A"
-ON_WEBSITE_NOT_SOLD = "O"
+ON_WEBSITE_SOLD = "O" # 공홈에도 있고 실제로 판매 중인 제품
+NOT_ON_WEBSITE_SOLD = "X" # 공홈에는 없지만 실제로 판매 중인 제품
+NOT_AVAILABLE = "N/A" # 공홈에도 없고 실제로 판매하지 않는 제품
+ON_WEBSITE_NOT_SOLD = "O" # 공홈에는 있지만 판매하지 않는 제품
 max_stores_to_save = 5
 
-current_dir = os.getcwd()
-datetime_now = datetime.now().strftime('%Y%m%d')
-source_filename = '루이비통.xlsx'
-target_filename = f'루이비통_{datetime_now}.xlsx'
-progress_file_name = f'progress_{datetime_now}.txt'
+current_dir = os.getcwd()  # 현재 디렉토리 경로를 가져오는 함수입니다.
+datetime_now = datetime.now().strftime('%Y%m%d')  # 현재 날짜와 시간을 가져오는 함수입니다.
+source_filename = '루이비통.xlsx'  # 가져올 엑셀 파일의 이름입니다.
+target_filename = f'루이비통_{datetime_now}.xlsx'  # 저장할 엑셀 파일의 이름입니다.
+progress_file_name = f'progress_{datetime_now}.txt'  # 진행 상태를 저장할 파일의 이름입니다.
 
-source_path = os.path.join(current_dir, source_filename)
-target_path = os.path.join(current_dir, target_filename)
-progress_file = os.path.join(current_dir, progress_file_name)
+source_path = os.path.join(current_dir, source_filename)  # 가져올 엑셀 파일의 전체 경로입니다.
+target_path = os.path.join(current_dir, target_filename)  # 저장할 엑셀 파일의 전체 경로입니다.
+progress_file = os.path.join(current_dir, progress_file_name)  # 진행 상태 파일의 전체 경로입니다.
 
+# 진행 상태를 저장하는 함수입니다.
 def save_progress(progress_file, sheet_name, row):
     with open(progress_file, 'w') as file:
         file.write(f"{sheet_name},{row}")
 
+# 진행 상태를 불러오는 함수입니다.
 def load_progress(progress_file):
     if os.path.exists(progress_file):
         with open(progress_file, 'r') as file:
@@ -88,8 +93,18 @@ def get_product_info(product_code):
             product_name = "N/A"
 
         try:
-            material = driver.find_element(By.CLASS_NAME, 'lv-product-variation-selector__value').text
-        except:
+            # 소재 정보를 포함하는 버튼 요소를 찾기
+            material_button = WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, "button.lv-product-variation-selector"))
+            )
+            # '소재'라는 텍스트를 가진 span 요소 찾기
+            material_title = material_button.find_element(By.XPATH, ".//span[contains(text(), '소재')]")
+
+            # 소재 값을 가진 span 요소 찾기
+            material_value = material_button.find_element(By.CSS_SELECTOR, '.lv-product-variation-selector__value').text
+            material = material_value.strip()
+        except Exception as e:
+            print(f"소재 정보 파싱 중 오류 발생: {e}")
             material = "N/A"
 
         try:
@@ -122,7 +137,7 @@ def get_product_info(product_code):
         return price_text, product_name, material, size_text, feature_text, description_text
 
     except Exception as e:
-        print(f"An error occurred with product {product_code}: {str(e)}")
+        print(f"제품 {product_code}에 대한 오류 발생: {str(e)}")
         return "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
 
 # 각 제품 코드에 대한 정보 가져오기
@@ -138,8 +153,8 @@ for sheet_name in workbook.sheetnames:
 
         product_code = row[0]
         product_price, product_name, material, size, features, description = get_product_info(product_code)
-        print(f"제품 코드: {product_code}, 제품명: {product_name}, 가격: {product_price}, 소재: {material}, 사이즈: {size}")
-        print(f"특징: {features}, 설명: {description}")
+        #print(f"제품 코드: {product_code}, 제품명: {product_name}, 가격: {product_price}, 소재: {material}, 사이즈: {size}")
+        #print(f"특징: {features}, 설명: {description}")
 
         # 매장 재고 조회 성공 후 처리
         total_stores_count = 0
@@ -199,8 +214,8 @@ for sheet_name in workbook.sheetnames:
                                 store_name = store.get('name')
                                 stock_info.append(store_name)
 
-                                # 서울/도산 매장 카운트
-                                if '서울' in store_name or '도산' in store_name:
+                                # 메종 서울/ 서울 도산 매장 카운트
+                                if '루이 비통 메종 서울' in store_name or '루이 비통 서울 도산' in store_name:
                                     seoul_dosan_count += 1
 
                         else:
@@ -211,9 +226,9 @@ for sheet_name in workbook.sheetnames:
                         continue
 
                 # 재고 정보 출력
-                print(f"총 {total_stores_count} 개 매장에서 재고를 찾았습니다.")
-                print(f"서울/도산 매장 중 {seoul_dosan_count} 개 매장에서 재고를 찾았습니다.")
-                print("재고 있는 매장:", stock_info)
+                #print(f"총 {total_stores_count} 개 매장에서 재고를 찾았습니다.")
+                #print(f"서울/도산 매장 중 {seoul_dosan_count} 개 매장에서 재고를 찾았습니다.")
+                #print("재고 있는 매장:", stock_info)
 
             except Exception as e:
                 print(f"재고 조회 중 오류 발생: {e}")
@@ -246,16 +261,44 @@ for sheet_name in workbook.sheetnames:
             website_status = ON_WEBSITE_SOLD
 
         # 결과를 엑셀 파일에 쓰기
-        sheet.cell(row=row_num, column=2, value=product_name)
-        sheet.cell(row=row_num, column=3, value=product_price)
-        sheet.cell(row=row_num, column=4, value=material)
-        sheet.cell(row=row_num, column=5, value=size)
-        sheet.cell(row=row_num, column=6, value=features)
-        sheet.cell(row=row_num, column=7, value=description)
-        sheet.cell(row=row_num, column=8, value=stock_status)
-        sheet.cell(row=row_num, column=9, value=website_status)  # 열 추가 및 공홈 등록 상태 기록
-        sheet.cell(row=row_num, column=10, value=stock_info_str)  # 재고 매장 정보 입력
+        sheet.cell(row=row_num, column=2, value=product_price) #매장가
+        sheet.cell(row=row_num, column=3, value=website_status) #공홈여부
+        sheet.cell(row=row_num, column=4, value=stock_status) #재고햔황
+        sheet.cell(row=row_num, column=5, value=stock_info_str)  #재고매장
+        sheet.cell(row=row_num, column=6, value=product_name) #제품명
+        sheet.cell(row=row_num, column=7, value=material) #소재
+        sheet.cell(row=row_num, column=8, value=size) #사이즈
+        sheet.cell(row=row_num, column=9, value=features) #특징
+        sheet.cell(row=row_num, column=10, value=description) #세부 설명
+        
+        # 제품 정보 출력
+        def print_product_info(product_code, product_price, website_status, stock_status, stock_info, product_name, material, size, features, description):
+            # 문자열을 주어진 너비에 맞춰 왼쪽으로 정렬하고 줄바꿈
+            def format_text_block(text, width=70):
+                return textwrap.fill(text, width=width)
 
+            # 세부 설명 포맷팅
+            formatted_description = format_text_block(description)
+            formatted_features = format_text_block(features)
+            
+            # 재고 매장 정보 포맷팅: 매장명을 반복하며 새로운 줄에 출력
+            formatted_stock_info = "\n".join(stock_info)  # 이 부분이 수정되었습니다.
+
+            print(Fore.LIGHTCYAN_EX + "[제품 정보]:" + Style.RESET_ALL)
+            print(Fore.LIGHTMAGENTA_EX + f"품번: {product_code}" + Style.RESET_ALL)
+            print(Fore.LIGHTGREEN_EX + f"매장가: {product_price}" + Style.RESET_ALL)
+            print(Fore.LIGHTYELLOW_EX + f"공홈여부: {website_status}" + Style.RESET_ALL)
+            print(Fore.LIGHTBLUE_EX + f"재고현황: {stock_status}" + Style.RESET_ALL)
+            print(Fore.LIGHTMAGENTA_EX + f"재고매장:\n{formatted_stock_info}" + Style.RESET_ALL)
+            print(Fore.LIGHTRED_EX + f"제품명: {product_name}" + Style.RESET_ALL)
+            print(Fore.LIGHTCYAN_EX + f"소재: {material}" + Style.RESET_ALL)
+            print(Fore.LIGHTGREEN_EX + f"사이즈: {size}" + Style.RESET_ALL)
+            print(Fore.LIGHTYELLOW_EX + f"특징:\n{formatted_features}" + Style.RESET_ALL)
+            print(Fore.LIGHTBLUE_EX + f"세부 설명:\n{formatted_description}" + Style.RESET_ALL)
+
+        # 이 함수를 호출하는 부분에서는 위 함수를 활용해 제품 정보를 출력합니다.
+        print_product_info(product_code, product_price, website_status, stock_status, stock_info, product_name, material, size, features, description)
+        
         # 저장
         workbook.save(filename=target_path)
         print(f"제품 정보를 엑셀 파일에 저장했습니다.")
