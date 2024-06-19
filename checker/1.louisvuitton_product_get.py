e44977c4f24b435873d9f35f8bd9c9b67d62acfd
djshin443 <132671614+djshin443@users.noreply.github.com>
2024-06-20 00:01:32 +0900

    Update 1.louisvuitton_product_get.py

--- checker/1.louisvuitton_product_get.py
+++ checker/1.louisvuitton_product_get.py
@@ -32,15 +32,59 @@ def create_driver():
 # 시작 URL 및 시트 이름 매핑
 start_urls = {
     "https://kr.louisvuitton.com/kor-kr/women/handbags/_/N-tfr7qdp": "여성 > 핸드백 전체",
-    "https://kr.louisvuitton.com/kor-kr/women/travel/travel-bags/_/N-tdxhh3p": "여성 > 트래블 > 러기지 전체",
-    "https://kr.louisvuitton.com/kor-kr/women/travel/travel-accessories/_/N-tbc3s3k": "여성 > 트래블 > 트래블 액세사리",
-    "https://kr.louisvuitton.com/kor-kr/women/small-leather-goods/long-wallets/_/N-t1wnwc4o": "여성 > 지갑가죽소품 > 장지갑",
-    "https://kr.louisvuitton.com/kor-kr/women/small-leather-goods/compact-wallets/_/N-ttz8v5o": "여성 > 지갑가죽소품 > 반 지갑",
-    "https://kr.louisvuitton.com/kor-kr/women/small-leather-goods/chain-and-strap-wallets/_/N-t11y8dds": "여성 > 지갑가죽소품 > 체인지갑",
-    "https://kr.louisvuitton.com/kor-kr/women/small-leather-goods/pouches/_/N-twzkl0p": "여성 > 지갑가죽소품 > 파우치",
-    "https://kr.louisvuitton.com/kor-kr/women/small-leather-goods/card-holders-and-key-holders/_/N-tngffj4": "여성 > 지갑가죽소품 > 카드 홀더",
-    "https://kr.louisvuitton.com/kor-kr/women/travel/travel-accessories/_/N-tbc3s3k": "여성 > 트래블 > 트래블 액세사리",
-    "https://kr.louisvuitton.com/kor-kr/bags/for-men/all-bags/_/N-t1z0ff7q": "남자 > 핸드백 전체"
+    "https://kr.louisvuitton.com/kor-kr/women/accessories/belts/_/N-ty346sh": "여성 > 액세서리 > 벨트" ,
+    "https://kr.louisvuitton.com/kor-kr/women/accessories/silk-squares-and-bandeaus/_/N-tte63om": "여성 > 액세서리 > 실크 스퀘어 및 방도",
+    "https://kr.louisvuitton.com/kor-kr/women/accessories/scarves-and-shawls/_/N-toyetr6": "여성 > 액세서리 > 숄 앤 스툴 ",
+    "https://kr.louisvuitton.com/kor-kr/women/accessories/scarves/_/N-tmr7ugu": "여성 > 액세서리 > 스카프",
+    "https://kr.louisvuitton.com/kor-kr/women/accessories/hair-accessories/_/N-t1ydxwto": "여성 > 액세서리 > 헤어 액세서리",
+    "https://kr.louisvuitton.com/kor-kr/women/accessories/key-holders-and-bag-charms/_/N-tui7c0x": "여성 > 액세서리 > 키홀더 n 백참",
+    
+    "https://kr.louisvuitton.com/kor-kr/women/fashion-jewelry/bracelets/_/N-t7sxnbc": "여성 > 패션 주얼리 > 팔찌",
+    "https://kr.louisvuitton.com/kor-kr/women/fashion-jewelry/rings/_/N-t175idqj": "여성 > 패션 주얼리 > 반지",
+    "https://kr.louisvuitton.com/kor-kr/women/fashion-jewelry/earrings/_/N-t1fwp6ww": "여성 > 패션 주얼리 > 귀걸이",
+    "https://kr.louisvuitton.com/kor-kr/women/fashion-jewelry/necklaces-and-pendants/_/N-tguaa6b": "여성 > 패션 주얼리 > 목걸이 n 펜던트",
+
+    "https://kr.louisvuitton.com/kor-kr/women/shoes/sneakers/_/N-t13nbknz": "여성 > 슈즈 > 스니커즈",
+    "https://kr.louisvuitton.com/kor-kr/women/shoes/boots-and-ankle-boots/_/N-t1is1ifz": "여성 > 슈즈 > 부츠 n 부티",
+    "https://kr.louisvuitton.com/kor-kr/women/shoes/loafers-and-ballerinas/_/N-t18l8jj3": "여성 > 슈즈 > 로퍼 n 발레리나",
+    "https://kr.louisvuitton.com/kor-kr/women/shoes/platform-shoes/_/N-twh7u2z": "여성 > 슈즈 > 플랫폼 슈즈",
+    "hhttps://kr.louisvuitton.com/kor-kr/women/shoes/mules-and-slides/_/N-t1lkbycc": "여성 > 슈즈 > 뮬 n 슬라이드",
+    "https://kr.louisvuitton.com/kor-kr/women/shoes/sandals-and-espadrilles/_/N-t1ey0zdr": "여성 > 슈즈 > 샌들",
+    "https://kr.louisvuitton.com/kor-kr/women/shoes/pumps/_/N-t1wpeudq": "여성 > 슈즈 > 펌프스",
+
+    "https://kr.louisvuitton.com/kor-kr/bags/for-men/all-bags/_/N-t1z0ff7q": "남성 > 핸드백 전체",
+
+    "https://kr.louisvuitton.com/kor-kr/men/accessories/belts/_/N-t1g9dx5w": "남성 > 액세서리 > 벨트",
+    "https://kr.louisvuitton.com/kor-kr/men/accessories/scarves/_/N-t186w8xz": "남성 > 액세서리 > 스카프",
+    "https://kr.louisvuitton.com/kor-kr/men/accessories/hats-and-gloves/_/N-t1fq44ws": "남성 > 액세서리 > 모자 n 장갑",
+    "https://kr.louisvuitton.com/kor-kr/men/accessories/ties-and-pocket-squares/_/N-tv1f2za": "남성 > 액세서리 > 타이 n 포켓 스퀘어",
+    "https://kr.louisvuitton.com/kor-kr/men/accessories/key-holders-and-bag-charms/_/N-tzumbbt": "남성 > 액세서리 > 키홀더 n 백참",
+
+    "https://kr.louisvuitton.com/kor-kr/men/fashion-jewelry/necklaces-and-pendants/_/N-t79dl9r": "남성 > 패션 주얼리 > 목걸이 n 펜던트",
+    "https://kr.louisvuitton.com/kor-kr/men/fashion-jewelry/bracelets/_/N-t17pp1v0": "남성 > 패션 주얼리 > 팔찌",
+    "https://kr.louisvuitton.com/kor-kr/men/fashion-jewelry/rings/_/N-t1l6jm7c": "남성 > 패션 주얼리 > 반지",
+    "https://kr.louisvuitton.com/kor-kr/men/fashion-jewelry/earrings/_/N-t1nhj986": "남성 > 패션 주얼리 > 귀걸이",
+
+    "https://kr.louisvuitton.com/kor-kr/men/shoes/sneakers/_/N-t1fud07g": "남성 > 슈즈 > 스니커즈",
+    "https://kr.louisvuitton.com/kor-kr/men/shoes/loafers-and-moccasins/_/N-t1x4h6di": "남성 > 슈즈 > 로퍼 n 모카신",
+    "https://kr.louisvuitton.com/kor-kr/men/shoes/lace-ups-and-buckles-shoes/_/N-tra4xg2": "남성 > 슈즈 > 레이스업",
+    "https://kr.louisvuitton.com/kor-kr/men/shoes/boots/_/N-tp5lgyh": "남성 > 슈즈 > 부츠",
+    "https://kr.louisvuitton.com/kor-kr/men/shoes/sandals/_/N-t1c9fsss": "남성 > 슈즈 > 샌들",
+
+    "https://kr.louisvuitton.com/kor-kr/men/wallets-and-small-leather-goods/long-wallets/_/N-th7ik7d": "남성 > 지갑가죽소품 > 장지갑",
+    "https://kr.louisvuitton.com/kor-kr/men/wallets-and-small-leather-goods/compact-wallets/_/N-teruo45": "남성 > 지갑가죽소품 > 반지갑",
+    "https://kr.louisvuitton.com/kor-kr/men/wallets-and-small-leather-goods/cardholders-and-passport-cases/_/N-t8v1ks": "남성 > 지갑가죽소품 > 카드 지갑 n 여권 지갑",
+    "https://kr.louisvuitton.com/kor-kr/men/wallets-and-small-leather-goods/wearable-wallets/_/N-t1yhvr0b": "남성 > 지갑가죽소품 > 웨어러블 가죽 소품",
+    "https://kr.louisvuitton.com/kor-kr/men/wallets-and-small-leather-goods/pouches/_/N-t1202gxy": "남성 > 지갑가죽소품 > 파우치",
+
+    "https://kr.louisvuitton.com/kor-kr/men/bags/leather-bags-selection/_/N-t1gza3m5": "남성 > 가방 > 레더백 셀렉션",
+    "https://kr.louisvuitton.com/kor-kr/men/bags/iconic-monogram-bags/_/N-taeo5qx": "남성 > 가방 > 아이코닉 모노그램 백",
+    "https://kr.louisvuitton.com/kor-kr/men/bags/damier-signature/_/N-tn65vv5": "남성 > 가방 > 다미에 시그니처",
+    "https://kr.louisvuitton.com/kor-kr/men/bags/backpacks/_/N-t7ipiak": "남성 > 가방 > 백팩",
+    "https://kr.louisvuitton.com/kor-kr/men/bags/crossbody-bags/_/N-t1bmxkvj": "남성 > 가방 > 크로스 백",
+    "https://kr.louisvuitton.com/kor-kr/men/bags/business-bags/_/N-t7dnshy": "남성 > 가방 > 비즈니스 백",
+    "https://kr.louisvuitton.com/kor-kr/men/bags/totes/_/N-t1ixjs8n": "남성 > 가방 > 토트 백",
+    "https://kr.louisvuitton.com/kor-kr/men/bags/small-bags-and-belt-bags/_/N-t1pizyia": "남성 > 가방 > 스몰 백 n 벨트백",
 }
 
 # 중복된 품번을 저장하기 위한 set 생성
@@ -99,7 +143,7 @@ def process_url(driver, start_url, sheet_name):
 
         if product_ids == last_product_ids:
             elapsed_time = time.time() - start_time
-            if elapsed_time >= 10:  # 10초 동안 새로운 품번이 없으면 다음 URL로 이동
+            if elapsed_time >= 60:  # 10초 동안 새로운 품번이 없으면 다음 URL로 이동
                 print(f"No new product IDs found for 10 seconds. Moving to the next URL.")
                 break
         else:
@@ -143,13 +187,11 @@ def main():
         # 각 시작 URL에 대해 페이지를 탐색
         for start_url, sheet_name in start_urls.items():
             process_url(driver, start_url, sheet_name)
+        print("All URLs have been processed. The program will now exit.")
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
         driver.quit()
 
 if __name__ == "__main__":
-    while True:
-        main()
-        print("Processing complete. Restarting in 5 seconds...")
-        time.sleep(5)
+    main()
